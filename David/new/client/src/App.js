298533fc029b35e1b810c62cc8b974c14644b5b7
david <he.xin@northeastern.edu>
2021-08-08 14:35:36 -0400

    Rearrange all the information into David's folder

---
+++ David/new/client/src/App.js
@@ -0,0 +1,14 @@
+import Chat from './Chat.js';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Chat />    
+    </div>
+  );
+}
+
+export default App;
